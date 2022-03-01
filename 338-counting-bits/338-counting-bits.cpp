ab73300dc980d85d023cc5d83f1d0ce750673b7d
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-03-01 11:00:31 +0530

    Time: 3 ms (95.58%), Space: 8.5 MB (21.58%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        int c=0;
+        vector<int>v;
+        for(int i=0;i<=n;i++)
+        {
+            v.push_back(__builtin_popcount(i));
+        }
+        return v;
+    }
+};
