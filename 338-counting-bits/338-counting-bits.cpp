4e39e0780978b39ed314e42cf1ca1681e0a13df1
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-03-01 11:26:09 +0530

    Time: 7 ms (62.32%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         int c=0;
-        vector<int>v;
-        for(int i=0;i<=n;i++)
+        vector<int>v(n+1);
+        v[0]=0;
+        for(int i=1;i<=n;i++)
         {
-            v.push_back(__builtin_popcount(i));
+            if(i%2==1)
+            {
+                v[i]=1+v[i/2]; 
+            }
+            else
+            {
+                v[i]=v[i/2];
+            }
         }
         return v;
     }
