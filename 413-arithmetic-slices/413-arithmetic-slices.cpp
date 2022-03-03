aaeee5201e58ededbe243733e99ba0f34be1f4e1
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-03-03 12:56:00 +0530

    Time: 6 ms (23.45%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& a) {
         
-        int n=a.size();
-        if(n<=2)
-        {
-            return 0;
-        }
-        int c=0;
-        vector<int>v(n,0);
-        v[0]=v[1]=0;
-        for(int i=2;i<n;i++)
-        {
-            if(a[i-1]-a[i-2]==a[i]-a[i-1])
-            {
-                v[i]=1+v[i-1];
-                c+=v[i];
-            }
-        }
-        
-        return c;
+          int ans=0;
+    int cnt=0;
+    for(int i=1;i<a.size()-1;i++)
+    {
+        if(a[i-1]-a[i]==a[i]-a[i+1])
+            ans++;
+        else
+            ans=0;
+        cnt+=ans;
+    }
+    
+   
+    
+    
+    return cnt;
         
     }
 };
