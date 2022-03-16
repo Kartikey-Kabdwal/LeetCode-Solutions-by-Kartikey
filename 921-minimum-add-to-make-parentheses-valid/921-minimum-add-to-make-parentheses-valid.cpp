f5558fc02edd5dfff12578466473a86a3a092a74
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-03-16 16:19:40 +0530

    Time: 3 ms (38.80%), Space: 6.4 MB (22.08%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        
-        int a=0,b=0;
+        int a,b;
+        a=b=0;
         stack<char>st;
         for(int i=0;i<s.size();i++)
         {
