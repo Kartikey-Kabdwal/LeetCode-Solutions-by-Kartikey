c13c43dbe0d453954dfa47038d9e8fff1817be46
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-03-24 22:10:00 +0530

    Time: 7 ms (40.23%), Space: 9 MB (51.61%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& v) {
+        sort(v.begin(),v.end());
+        int d=v[1]-v[0];
+        for(int i=2;i<v.size();i++)
+        {
+            if(v[i]-v[i-1]!=d)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
