2e743774c20838239cd9803360bc1fcccfc17457
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-04-03 22:23:04 +0530

    Time: 31 ms (17.32%), Space: 15.9 MB (71.39%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>&  v, char k) {
         
-        sort(v.begin(),v.end());
-        for(auto it:v)
-        {
-            if(it>k)
-            {
-                return it;
-            }
-        }
-        return v[0];
+        auto it=upper_bound(v.begin(),v.end(),k);
+            return it==v.end()?v[0]:*it;
     }
 };
