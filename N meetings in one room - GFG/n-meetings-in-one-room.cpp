7493722280eaa56eae89e04964531b0dd41ed487
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-04-04 11:59:18 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -4,42 +4,47 @@ using namespace std;
 
  // } Driver Code Ends
 
-bool func(pair<int,int>x,pair<int,int>y)
-{   if(x.second==y.second)
-{
-    return x.first<y.first;
-}
-    return x.second<y.second;
-}
+ bool func(pair<int,int>a,pair<int,int>b)
+    {
+        if(a.second==b.second)
+        {
+            return a.first<b.first;
+        }
+        return a.second<b.second;
+        }
 class Solution
 {
+    
     public:
+    
+    
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
     int maxMeetings(int s[], int e[], int n)
     {
-       
+        // Your code here
+        
         vector<pair<int,int>>v;
         for(int i=0;i<n;i++)
         {
-            v.push_back({s[i],e[i]}); 
-            
+            v.push_back({s[i],e[i]});
         }
         sort(v.begin(),v.end(),func);
-       int i=0,j=1,c=1;
-       while(j<n)
-       {
-           if(v[i].second<v[j].first)
-           {
-               c++;
-               i=j;
-               j++;
-           }
-           else
-           {
-               j++;
-           }
-       }
+        int c=1;
+        int i=0,j=1;
+        while(j<n)
+        {
+            if(v[j].first>v[i].second)
+            {
+                c++;
+                i=j;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
         return c;
     }
 };
