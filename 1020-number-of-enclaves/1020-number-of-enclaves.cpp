eb44b96049a704c203820674e6eb93db8726d8c7
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-07-10 10:45:34 +0530

    Time: 104 ms (40.51%), Space: 21.7 MB (86.70%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -15,12 +15,12 @@ public:
     int numEnclaves(vector<vector<int>>& v) {
         int n=v.size();
         int m=v[0].size();
-        int c=0;
+        int count=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(v[i][j]==1 and i==0 or j==0 or i==n-1 or j==m-1)
+                if(v[i][j]==1 and (i==0 or j==0 or i==n-1 or j==m-1))
                 {
                     dfs(v,i,j);
                 }
@@ -33,11 +33,11 @@ public:
                 if(v[i][j]==1)
                 {
                     
-                    c++;
+                    count++;
                 }
             }
         }
-        return c;
+        return count;
         
     }
 };
