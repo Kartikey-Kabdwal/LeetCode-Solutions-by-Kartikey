f127c76eac9cfdb74b705ef919a9ff341bfcbaf3
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-07-31 17:44:55 +0530

    Time: 383 ms (5.01%), Space: 48.1 MB (5.75%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    // Time Complexity:- O(nlogn)
-    // Space Complexity:- O(n)
+   
+    static bool func(vector<int>a,vector<int>b)
+    {
+        
+        if(a[0]==b[0])
+        {
+            return a[1]<b[1];
+        }
+ 
+            return a[0]<b[0];
+        
+    }
     vector<vector<int>> merge(vector<vector<int>>& v) {
-        sort(v.begin(),v.end());
-        
-        vector<vector<int>> ans = {v[0]};
-        
-        for(int i=1;i<v.size();i++){
-            
-            if(v[i][0]>ans.back()[1]) // intervals can't be merged
-             {     
-                ans.push_back(v[i]);
-             }
+        sort(v.begin(),v.end(),func);
+       vector<vector<int>>ans;
+        ans.push_back(v[0]);
+        for(int i=1;i<v.size();i++)
+        {
+           if(ans.back()[1]<v[i][0])
+           {
+               ans.push_back(v[i]);
+           }
             else
             {
-                ans.back()[1] = max(ans.back()[1],v[i][1]); // intervals can be merged
-            }
+                ans.back()[1]=max(ans.back()[1],v[i][1]);
+            }  
         }
         return ans;
+        
+        
     }
 };
