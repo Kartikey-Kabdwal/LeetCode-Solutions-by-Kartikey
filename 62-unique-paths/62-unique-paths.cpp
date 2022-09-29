e88c8166917f5659c6f9d8aeac2a05f7068493e6
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-09-29 12:37:50 +0530

    Time: 3 ms (44.94%), Space: 6 MB (75.78%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,38 +5,30 @@ public:
     
     int dp[105][105];
     
-    int helper(int i, int j, int n, int m)
-    {
-        // if target is reached
-        
-        if(i == n - 1 && j == m - 1)
-            return 1;
-        
-        // if already calculated
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int paths = 0;
-        
-        // possible paths from down side
-        
-        if(i + 1 < n)
-        {
-            paths += helper(i + 1, j, n, m);
-        }
-        
-        // possible paths from right side
-        
-        if(j + 1 < m)
-        {
-            paths += helper(i, j + 1, n, m);
-        }
-        
-        // store the result in dp and return it
-        
-        return dp[i][j] = paths;
-    }
+   int helper(int i, int j, int n, int m)
+     {
+         
+          if (i >= n or j >= m)
+          {
+               return 0;
+          }
+          if (i == n - 1 && j == m - 1)
+               return 1;
+
+      
+          if (dp[i][j] != -1)
+               return dp[i][j];
+
+          int paths = 0;
+
+       
+          paths += helper(i + 1, j, n, m);
+
+          paths += helper(i, j + 1, n, m);
+
+     
+          return dp[i][j] = paths;
+     }
     
     int uniquePaths(int n, int m) {
         
