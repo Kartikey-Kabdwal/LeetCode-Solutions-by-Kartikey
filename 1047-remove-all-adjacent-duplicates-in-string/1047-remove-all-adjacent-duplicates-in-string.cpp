6e5361a2e810b42c7dde5267cd5f9af7800f7fa5
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-11-24 13:28:36 +0530

    Time: 34 ms (72.66%), Space: 10.9 MB (78.40%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    string removeDuplicates(string S) {
-        string a;
-        for (auto c : S) 
-            if (a.size() && a.back() == c) a.pop_back();
-            else a.push_back(c);
-        return a;
-    }
+    string removeDuplicates(string S)
+{
+    string res = "";
+    for (auto c : S)
+        if (res.size() && c == res.back())
+            res.pop_back();
+        else
+            res.push_back(c);
+    return res;
+}
 };
