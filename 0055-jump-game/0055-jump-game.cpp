497b12687119620684d27f7a443fd059ed683393
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-12-26 08:45:02 +0530

    Time: 58 ms (98.02%), Space: 48.5 MB (48.80%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& v) {
+        int n=v.size();
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            if(j<i)
+            {
+                return false;
+            }
+            j=max(j,i+v[i]);
+            
+        }
+        
+        return true;
+    }
+};
