8e918c44fd31ef187a1f9e2ab199b00cd63dde14
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-01-12 15:10:01 +0530

    Time: 1317 ms (9.10%), Space: 237.8 MB (5.10%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     static bool func(vector<int>a,vector<int>b)
     {
-         int x=(a[0]*a[0])+(a[1]*a[1]);
-         int y=(b[0]*b[0])+(b[1]*b[1]);
-        return x<y;
+        return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
     }
     vector<vector<int>> kClosest(vector<vector<int>>& v, int k) {
+        
         sort(v.begin(),v.end(),func);
         vector<vector<int>>ans;
         for(int i=0;i<k;i++)
@@ -14,7 +13,6 @@ public:
             ans.push_back(v[i]);
         }
         return ans;
-        
         
     }
 };
