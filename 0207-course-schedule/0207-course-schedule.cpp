6f53b8fa9bbdf33d572a40eefde9f5361ebe8441
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-01-20 19:00:12 +0530

    Time: 29 ms (57.21%), Space: 14.3 MB (40.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // Create a graph with the courses as vertices and the prerequisites as edges
-    unordered_map<int, vector<int>> graph;
-    for (const auto& p : prerequisites) {
-        graph[p[0]].push_back(p[1]);
-    }
 
-    // Create a vector to store the indegree of each course
-    vector<int> indegree(numCourses, 0);
-    for (const auto& p : graph) {
-        for (const auto& v : p.second) {
-            indegree[v]++;
-        }
-    }
+bool isCycle(vector<int> g[], int i, vector<int> &vis) {
+	if (vis[i] == 1) return true;
+    if (vis[i] == 2) return false;
+	if (vis[i] == 0) {
+		vis[i] = 1;
+		for (auto e : g[i]) {
+			if (isCycle(g, e, vis)) return true;
+		}
+	}
+	vis[i] = 2;
+	return false;
+}
 
-    // Create a queue to store the courses with no dependencies (indegree = 0)
-    queue<int> q;
-    for (int i = 0; i < numCourses; i++) {
-        if (indegree[i] == 0) {
-            q.push(i);
-        }
-    }
-
-    // Perform a topological sort
-    int count = 0;
-    while (!q.empty()) {
-        int course = q.front();
-        q.pop();
-        count++;
-
-        // Decrease the indegree of the courses that depend on the current course
-        for (const auto& v : graph[course]) {
-            indegree[v]--;
-            if (indegree[v] == 0) {
-                q.push(v);
-            }
-        }
-    }
-
-    // If all courses were visited, it means that it is possible to finish all the courses
-    return count == numCourses;
-        
-    }
+bool canFinish(int n, vector<vector<int>>& prerequisites) {
+	vector<int> g[n];
+	for (auto e : prerequisites) {
+		g[e[1]].push_back(e[0]);
+	}
+	vector<int> vis(n, 0);
+	for (int i = 0; i < n; ++i) {
+		if (isCycle(g, i, vis)) return false;
+	}
+	return true;
+}
 };
