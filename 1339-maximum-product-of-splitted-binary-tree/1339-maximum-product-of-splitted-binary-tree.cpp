dea0a065cb06fad989a06f3408b20da3b4477a5b
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-01-27 11:43:23 +0530

    Time: 239 ms (50.06%), Space: 77.4 MB (72.51%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,27 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    long res = 0, total = 0, sub;
+    long long int MOD = 1e9+7;
+    long long totalSum =0, ans=0;
+    int find(TreeNode*root)
+    {
+    if(root == NULL)
+     {
+        return 0;
+     }
+     long long sum =find(root->left) + find(root->right) + root->val;
+     ans = max(ans,(totalSum-sum)*sum );
+     return sum;
+    }
     int maxProduct(TreeNode* root) {
-        total = s(root), s(root);
-        return res % (int)(1e9 + 7);
-    }
+     totalSum = find(root);
+     find(root);
+     return ans%MOD;
 
-    int s(TreeNode* root) {
-        if (!root) return 0;
-        sub = root->val + s(root->left) + s(root->right);
-        res = max(res, sub * (total - sub));
-        return sub;
     }
-
 };
