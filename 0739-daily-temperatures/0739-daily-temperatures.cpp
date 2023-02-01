e13e7e50a8e29ac4680079938079aeca2a8e222b
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-02-01 23:14:33 +0530

    Time: 235 ms (67.23%), Space: 107.6 MB (12.07%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,28 @@
-class Solution {
-public:
-   vector<int> dailyTemperatures(vector<int>& temperatures) {
-    vector<int> ans(temperatures.size());
-    stack<int> st;
-    for (int i = temperatures.size() - 1; i >= 0; i--) {
-        while (!st.empty() && temperatures[i] >= temperatures[st.top()]) {
-            st.pop();
+class Solution
+{
+    public:
+        vector<int> dailyTemperatures(vector<int> &v)
+        {
+            stack<pair<int, int>> s;
+            int n = v.size();
+            vector<int> ans(n,0);
+            for (int i = 0; i < n; i++)
+            {
+                
+                while (!s.empty() and v[i] > s.top().first)
+                {
+                    ans[s.top().second] = i - s.top().second;
+                    s.pop();
+                      
+                }
+              
+                s.push({ v[i],i });
+            }
+            while (!s.empty())
+            {
+                ans[s.top().second] = 0;
+                s.pop();
+            }
+            return ans;
         }
-        ans[i] = st.empty() ? 0 : st.top() - i;
-        st.push(i);
-    }
-    return ans;
-}
-
 };
