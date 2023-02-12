b4262ee00f05b1cf47c6c93038f6a750e8c932fc
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-02-12 11:46:31 +0530

    Time: 574 ms (93.58%), Space: 191.3 MB (77.66%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,23 +1,32 @@
 class Solution {
-public:
-    long long dfs(int i, int prev, int depth, int seats, vector<vector<int>>& al, long long &res) {
-    long long riders = 1;
-    for (int j : al[i])
-        if (j != prev)
-            riders += dfs(j, i, depth + 1, seats, al, res);
-    if (depth == 0)
-        return res;
-    res += (riders / seats) * depth + (riders % seats ? 1 : 0);
-    return riders % seats;
-}
-long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-    vector<vector<int>> al(roads.size() + 1);
-    for (auto &r : roads) {
-        al[r[0]].push_back(r[1]);
-        al[r[1]].push_back(r[0]);
-    }
-    long long res = 0;
-    return dfs(0, -1, 0, seats, al, res);
-}
+ public:
+  long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+    long long ans = 0;
+    vector<vector<int>> graph(roads.size() + 1);
 
+    for (const vector<int>& road : roads) {
+      const int u = road[0];
+      const int v = road[1];
+      graph[u].push_back(v);
+      graph[v].push_back(u);
+    }
+
+    dfs(graph, 0, -1, seats, ans);
+    return ans;
+  }
+
+ private:
+  int dfs(const vector<vector<int>>& graph, int u, int prev, int seats,
+          long long& ans) {
+    int people = 1;
+    for (const int v : graph[u]) {
+      if (v == prev)
+        continue;
+      people += dfs(graph, v, u, seats, ans);
+    }
+    if (u > 0)
+      // # of cars needed = ceil(people / seats)
+      ans += (people + seats - 1) / seats;
+    return people;
+  }
 };
