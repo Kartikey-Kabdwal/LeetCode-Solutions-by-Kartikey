9618062d7a31ba1edf7f336ef5bab4491b09a746
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-02-22 12:41:47 +0530

    Time: 156 ms (70.21%), Space: 20.4 MB (39.26%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    int dikstra(vector<vector<int>>& a, vector<vector<int>>& dist)
-    {
-        int n=a.size(), m=a[0].size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0,0,0});
-        dist[0][0] = 0;
-        while(!pq.empty())
-        {
-            int i = pq.top()[0];
-            int j = pq.top()[1];
-            int w = pq.top()[2];
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+        int ans = 0;
+        using pi = pair<int, pair<int,int>>;
+        priority_queue<pi, vector<pi>,  greater<pi>> pq;
+        pq.push({0, {0,0}});
+        
+        while(!pq.empty())  {
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            int esf = pq.top().first;
             pq.pop();
             
-            if(i>0 && dist[i-1][j]>max(w,abs(a[i][j]-a[i-1][j])) )
-            {
-                dist[i-1][j] = max(w,abs(a[i][j]-a[i-1][j]));
-                pq.push({i-1,j,dist[i-1][j]});
-            }
-            if(i<n-1 && dist[i+1][j]>max(w,abs(a[i][j]-a[i+1][j])) )
-            {
-                dist[i+1][j] = max(w,abs(a[i][j]-a[i+1][j]));
-                pq.push({i+1,j,dist[i+1][j]});
-            }
-            if(j>0 && dist[i][j-1]>max(w,abs(a[i][j]-a[i][j-1])) )
-            {
-                dist[i][j-1] = max(w,abs(a[i][j]-a[i][j-1]));
-                pq.push({i,j-1,dist[i][j-1]});
-            }
-            if(j<m-1 && dist[i][j+1]>max(w,abs(a[i][j]-a[i][j+1])) )
-            {
-                dist[i][j+1] = max(w,abs(a[i][j]-a[i][j+1]));
-                pq.push({i,j+1,dist[i][j+1]});
-            }
+            if(heights[x][y]==-1)
+                continue;
+            int ht = heights[x][y];
+            heights[x][y] = -1;
+            ans = max(ans, esf);
+            
+            if(x==n-1 && y==m-1)
+                return ans;
+            
+           if(x-1>=0 && heights[x-1][y]!=-1)
+               pq.push({abs(ht-heights[x-1][y]), {x-1, y}});
+           if(y-1>=0 && heights[x][y-1]!=-1)
+               pq.push({abs(ht-heights[x][y-1]), {x, y-1}});
+           if(x+1<=n-1 && heights[x+1][y]!=-1)
+               pq.push({abs(ht-heights[x+1][y]), {x+1, y}});
+           if(y+1<=m-1 && heights[x][y+1]!=-1)
+               pq.push({abs(ht-heights[x][y+1]), {x, y+1}});
+            
         }
-        
-        return dist[n-1][m-1];
-    }
-    
-    int minimumEffortPath(vector<vector<int>>& a) {
-        int n=a.size(), m=a[0].size();
-        vector<vector<int>> dist(n, vector<int>(m,INT_MAX));
-        int ans = dikstra(a,dist);
-        return ans;
+        return 0;
     }
 };
